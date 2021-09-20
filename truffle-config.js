ae728e1b81f1435a98b1e2f114ef412c7006e2fc
Elias Bundala <ebundala@gmail.com>
2021-09-20 14:09:35 +0300

    chore(truffle) add truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -5,22 +5,29 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //}
+  networks: {
+   development: {
+     host: "127.0.0.1",
+     port: 7545,
+     network_id: "*"
+   },
+   test: {
+     host: "127.0.0.1",
+     port: 7545,
+     network_id: "*"
+   }
+  },
   //
   compilers: {
     solc: {
-      version: "0.6.12"
+      version: "0.6.12",
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 1000
+        },
+        evmVersion: "istanbul"
+       }
     }
   }
 };

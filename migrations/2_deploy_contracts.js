1bec87c9ea070cbb548c229b6914f3a03a00dc59
Elias Bundala <ebundala@gmail.com>
2021-11-05 15:43:08 +0300

    feat(token) Token created

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,1 +1,15 @@
-module.exports = function(deployer) {};
+const mockBep20 = artifacts.require('MockBEP20');
+
+
+const fs = require('fs');
+const path = require('path');
+const ethers = require("ethers")
+const supply = "500000000000000000000000000"//ethers.utils.formatUnits("500000000000000000000000000",'18')
+console.log(`Deploying MockBEP20 with supply ${supply.toString()}`)
+
+module.exports = async function(deployer, network, accounts) {
+    
+ await  deployer.deploy(mockBep20,"Ini Token","INI",supply);
+const contract = await mockBep20.deployed();
+fs.writeFileSync(path.join(__dirname, '../mockbep.txt'), contract.address);
+};
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+ const fs = require('fs');
+ const privateKey = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   // Uncommenting the defaults below
   // provides for an easier quick-start with Ganache.
@@ -6,6 +10,14 @@ module.exports = {
   // for more details on how to specify configuration options!
   //
   networks: {
+    binanceTestnet: {
+      network_id:97,
+      provider: () => new HDWalletProvider({
+        privateKeys:[privateKey], 
+        chainId:97,
+        providerOrUrl:`https://data-seed-prebsc-1-s1.binance.org:8545/`
+      }),
+    },
    development: {
      host: "127.0.0.1",
      port: 7545,
